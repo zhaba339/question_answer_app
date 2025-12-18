6d71969dc08bc53d4c9f5c7cbc80ab46ecd7b8e1
Alex Galinurov <74530611+zhaba339@users.noreply.github.com>
2025-12-18 23:49:46 +0600

    1. Дополнил модель Users полями login и password 2. Добавил отсутствующий файл script.py.mako для миграций

--- app/users/models.py
+++ app/users/models.py
@@ -8,4 +8,6 @@ class Users(Base):
 
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     user_id = sa.Column(sa.UUID, unique=True, nullable=False)
+    login = sa.Column(sa.String, nullable=False)
+    password = sa.Column(sa.String, nullable=False)
 
