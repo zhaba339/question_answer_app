c89ecd2dbac3a1a6a43eebe2a8e97d195f2a6a1d
AleksandrGalinurov <alexgalinurov@gmail.com>
2025-12-26 17:02:33 +0600

    добавил редис на получение одного ответа

--- app/answers/service.py
+++ app/answers/service.py
@@ -1,5 +1,7 @@
 from typing import List
 from uuid import UUID
+
+from app.redis_client import redis_client
 from app.answers.schemas import AnswerSchema
 from app.answers.repository import AnswerRepository
 
@@ -18,8 +20,20 @@ class AnswerService:
 
     @staticmethod
     async def get_one_answer(answer_id: int) -> AnswerSchema:
+        cached_key = f"answer:{answer_id}"
+        cached_data = await redis_client.get(cached_key)
+
+        if cached_data:
+            answer_json = AnswerSchema.model_validate_json(cached_data)
+            return answer_json
+
         answer = await AnswerRepository.find_one_answer(answer_id)
-        return answer
+
+        if answer:
+            answer_schema = AnswerSchema.model_validate(answer)
+            answer_json = answer_schema.model_dump_json()
+            await redis_client.setex(cached_key, 10, answer_json)
+            return answer
 
     @staticmethod
     async def delete_one_answer(answer_id: int) -> None:
