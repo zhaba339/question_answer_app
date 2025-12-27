1197041dd0a9eda3ead5203e0018fe3e26ffd415
Alex Galinurov <74530611+zhaba339@users.noreply.github.com>
2025-12-28 00:48:22 +0600

    Исправил сервис пользователей

--- app/users/service.py
+++ app/users/service.py
@@ -12,11 +12,15 @@ class UserService:
     @staticmethod
     async def get_all_users() -> List[UserSchema]:
         users = await UserRepository.find_all_users()
+        if not users:
+            return []
         return users
 
     @staticmethod
     async def get_one_user(user_id: int) -> UserSchema:
         user = await UserRepository.find_user(user_id)
+        if not user:
+            raise EntityNotFoundError
         return user
 
     @staticmethod
@@ -26,10 +30,12 @@ class UserService:
 
     @staticmethod
     async def delete_one_user(user_id) -> bool:
-        user_exists = await UserRepository.exists_user(user_id)
-        if not user_exists:
+        user = await UserRepository.find_user(user_id)
+        if not user:
             raise EntityNotFoundError("User", user_id)
-        if await UserRepository.exists_by_user(user_id):
+        user_has_content = await UserRepository.exists_answers_questions(user_id)
+        if user_has_content is True:
             raise EntityHasDependenciesError("User has existing answers or questions")
-        await UserRepository.delete(user_id)
-        return True
+        else:
+            await UserRepository.delete(user_id)
+            return True
